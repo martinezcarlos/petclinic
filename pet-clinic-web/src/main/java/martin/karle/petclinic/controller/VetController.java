22a22acbd3ad3811f4a70ac97339b337e1929db9
Karle <ingeniero.carlos.martinez@gmail.com>
2018-08-17 18:04:06 +0200

    Vets page. Closes #7

--- pet-clinic-web/src/main/java/martin/karle/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/martin/karle/petclinic/controller/VetController.java
@@ -1,6 +1,8 @@
 package martin.karle.petclinic.controller;
 
+import martin.karle.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+  private final VetService vetService;
+
+  public VetController(final VetService vetService) {
+    this.vetService = vetService;
+  }
+
   @RequestMapping({"", "/", "/index", "/index.html"})
-  public String listVets() {
+  public String listVets(final Model model) {
+    model.addAttribute("vets", vetService.findAll());
     return "vets/index";
   }
 }
