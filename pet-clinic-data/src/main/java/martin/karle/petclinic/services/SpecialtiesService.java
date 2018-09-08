632ec3d56afb1147b837acd68e9a740ea08c2dcb
martinezcarlos <ingeniero.carlos.martinez@gmail.com>
2018-09-08 20:25:39 +0200

    - Specialties sevice implemented. Closes #21

---
+++ pet-clinic-data/src/main/java/martin/karle/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,9 @@
+package martin.karle.petclinic.services;
+
+import martin.karle.petclinic.model.Specialty;
+
+/**
+ * Created by carlosmartinez on 8/9/18 20:11
+ */
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/martin/karle/petclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,39 @@
+package martin.karle.petclinic.services.map;
+
+import java.util.Set;
+import martin.karle.petclinic.model.Specialty;
+import martin.karle.petclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by carlosmartinez on 8/9/18 20:13
+ */
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long>
+    implements SpecialtiesService {
+
+  @Override
+  public Set<Specialty> findAll() {
+    return super.findAll();
+  }
+
+  @Override
+  public Specialty findById(final Long id) {
+    return super.findById(id);
+  }
+
+  @Override
+  public Specialty save(final Specialty object) {
+    return super.save(object);
+  }
+
+  @Override
+  public void deleteById(final Long id) {
+    super.deleteById(id);
+  }
+
+  @Override
+  public void delete(final Specialty object) {
+    super.delete(object);
+  }
+}
