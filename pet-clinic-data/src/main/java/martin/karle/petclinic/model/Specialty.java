30b9602a02a9e686e5684e022e290524a3b02291
martinezcarlos <ingeniero.carlos.martinez@gmail.com>
2018-09-08 14:08:26 +0200

    - Added Specialities and relationships. Closes #17

---
+++ pet-clinic-data/src/main/java/martin/karle/petclinic/model/Specialty.java
@@ -0,0 +1,12 @@
+package martin.karle.petclinic.model;
+
+import lombok.Data;
+
+/**
+ * Created by carlosmartinez on 8/9/18 13:57
+ */
+@Data
+public class Specialty extends BaseEntity {
+  
+  private String description;
+}
--- pet-clinic-data/src/main/java/martin/karle/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/martin/karle/petclinic/model/Vet.java
@@ -1,4 +1,10 @@
 package martin.karle.petclinic.model;
 
+import java.util.Set;
+import lombok.Data;
+
+@Data
 public class Vet extends Person {
+
+  private Set<Specialty> specialties;
 }
