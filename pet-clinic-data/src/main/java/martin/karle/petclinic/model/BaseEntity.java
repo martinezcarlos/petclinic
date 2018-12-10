fac3004d4845fa57152c5416a656587811f9a927
martinezcarlos <ingeniero.carlos.martinez@gmail.com>
2018-12-10 14:02:33 +0100

    - Updated base entity with JPA annotations. Closes #25

--- pet-clinic-data/src/main/java/martin/karle/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/martin/karle/petclinic/model/BaseEntity.java
@@ -1,13 +1,20 @@
 package martin.karle.petclinic.model;
 
 import java.io.Serializable;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import lombok.Data;
 
 /**
  * Created by carlosmartinez on 5/8/18 21:35
  */
+@MappedSuperclass
 @Data
 public class BaseEntity implements Serializable {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 }
