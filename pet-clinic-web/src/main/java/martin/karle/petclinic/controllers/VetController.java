5b83b9c086bba621dba38981ff7d4adce8c24c66
martinezcarlos <ingeniero.carlos.martinez@gmail.com>
2019-03-20 12:20:05 +0100

    - Added JSON endpoint for Vets. Closes #49.

--- pet-clinic-web/src/main/java/martin/karle/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/martin/karle/petclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package martin.karle.petclinic.controllers;
 
+import java.util.Set;
+import martin.karle.petclinic.model.Vet;
 import martin.karle.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by carlosmartinez on 11/vets/8/vets/18 1:02
@@ -15,6 +19,12 @@ public class VetController {
 
   public VetController(final VetService vetService) {
     this.vetService = vetService;
+  }
+
+  @GetMapping("/api/vets")
+  @ResponseBody
+  public Set<Vet> getVetsJson() {
+    return vetService.findAll();
   }
 
   @RequestMapping({
